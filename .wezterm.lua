066831369699c880b9e5794eba8d8c8f23c0ac3b
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2024-11-26 10:55:50 +0100

    Update wezterm
    Signed-off-by: FedericoBruzzone <federico.bruzzone.i@gmail.com>

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,7 +7,6 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 config.font = wezterm.font 'Iosevka'
-config.window_decorations = 'NONE' -- In arch with KDE resize with win+right click
 config.scrollback_lines = 10000
 config.enable_scroll_bar = true
 config.adjust_window_size_when_changing_font_size = false
